83796f3e8da2e56c02f65f174e73c4b0ab9c1965
vivek <chimnanivivek14@gmail.com>
2024-06-09 11:12:01 +0530

    used variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:second_app/text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
@@ -12,8 +15,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 46, 5, 117),
             Color.fromARGB(255, 46, 23, 86)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
