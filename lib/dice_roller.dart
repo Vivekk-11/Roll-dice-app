a276dd891b31c49ee049b6315c056e3393790fc2
vivek <chimnanivivek14@gmail.com>
2024-06-11 11:54:08 +0530

    created a stateful widget

---
+++ lib/dice_roller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeImage = "assets/images/dice-1.png";
+
+  void roleDice() {
+    setState(() {
+      activeImage = "assets/images/dice-2.png";
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeImage,
+          width: 200,
+        ),
+        TextButton(
+          onPressed: roleDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text("Click me!"),
+        )
+      ],
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,54 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/dice_roller.dart';
 // import 'package:second_app/text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.colors});
+  const GradientContainer({super.key, required this.colors});
 
-  GradientContainer.purple({super.key})
+  const GradientContainer.purple({super.key})
       : colors = const [Colors.deepPurple, Colors.indigo];
 
   final List<Color> colors;
 
-  var activeImage = "assets/images/dice-1.png";
-
-  void roleDice() {
-    activeImage = "assets/images/dice-2.png";
-    print("Changed Image!");
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: colors,
-          begin: startAlignment,
-          end: endAlignment,
-        ),
-      ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            activeImage,
-            width: 200,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: startAlignment,
+            end: endAlignment,
           ),
-          TextButton(
-            onPressed: roleDice,
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.only(top: 20),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const Text("Click me!"),
-          )
-        ],
-      )),
-      // child: TextContainer("HELLOO!!")),
-    );
+        ),
+        child: const Center(
+          child: DiceRoller(),
+          // child: TextContainer("HELLOO!!")),
+        ));
   }
 }
